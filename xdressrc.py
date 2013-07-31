080007c096f5e84f707387d12e6066e43f93f21e
Anthony Scopatz <scopatz@gmail.com>
2013-07-30 19:28:01 -0500

    more updates

--- xdressrc.py
+++ xdressrc.py
@@ -1,6 +1,17 @@
-sourcedir = "~/moab/itaps/imesh"
+sourcedir = "../moab/itaps/imesh"
 package = "imp"
 
+includes = ["/home/scopatz/.local/include", "/home/scopatz/moab/src"]
+
+undefines = ["__cplusplus"]
+
+functions = [
+    ('iMesh_dtor', 'iMesh_MOAB', 'iMesh'),
+    ('iMesh_getRootSet', 'iMesh_MOAB', 'iMesh'),
+    ]
+
 classes = [
+    ('iMesh_Instance', 'iMesh', 'iMesh'),
+    ('iMesh_Instance_Private', 'iMesh', 'iMesh'),
     ('iMeshArrayManager', 'iMesh_MOAB', 'iMesh'),
     ]
